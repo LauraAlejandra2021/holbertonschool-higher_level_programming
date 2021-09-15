c89aef9575df935fb2ede998468e295091232d41
LauraAlejandra2021 <3450@holbertonschool.com>
2021-09-15 11:30:31 -0500

    pto6.7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for x in range(i + 1, 10):
         if i == 8 and x == 9:
-            print("{:d}{:d} ".format(i, x))
+            print("{:d}{:d}".format(i, x))
         else:
             print("{:d}{:d}, ".format(i, x), end="")
