8533009baef7c589bc5720c4fe7968e6a71919cc
LauraAlejandra2021 <3450@holbertonschool.com>
2021-09-28 22:50:24 -0500

    square constructor and error handling

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""square constructor and error handling"""
+
+
+class Square:
+    """square constructor"""
+
+    def __init__(self, size=0):
+        """ private square constructor and error handling """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer ")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
