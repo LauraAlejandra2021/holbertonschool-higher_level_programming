f9a0a27b52be9e6468ee90dca289e0f51715dca6
LauraAlejandra2021 <3450@holbertonschool.com>
2021-09-28 23:23:16 -0500

    square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""square area"""
+
+
+class Square:
+    """Square area"""
+    def area(self):
+        """Method that returns the area """
+        return self.__size **2
+
+    def __init__(self, size=0):
+        """ private square constructor and error handling"""
+
+        self.__size = size 
+        if type(size) != int:
+            raise TypeError("size must be an integer ")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
