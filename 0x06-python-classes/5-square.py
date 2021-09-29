d08bae6e56dc9fc63f050be92ec0db6a94c4fb73
LauraAlejandra2021 <3450@holbertonschool.com>
2021-09-29 00:09:48 -0500

    avance point 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""square area"""
+
+
+class Square:
+    """Class crate  a Square """
+    @property
+    def size(self):
+        """ return a size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """get size"""
+        self.__size = value
+
+    def area(self):
+        """Method that returns the area """
+        return self.__size ** 2
+
+    def __init__(self, size=0):
+        """ private square constructor and error handling"""
+
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer ")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
