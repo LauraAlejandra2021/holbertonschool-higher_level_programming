791d14afaac8e2132561373079275c3f975cd9a3
LauraAlejandra2021 <3450@holbertonschool.com>
2021-09-28 23:33:08 -0500

    pycodestyle punto 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,12 +6,12 @@ class Square:
     """Square area"""
     def area(self):
         """Method that returns the area """
-        return self.__size **2
+        return self.__size ** 2
 
     def __init__(self, size=0):
         """ private square constructor and error handling"""
 
-        self.__size = size 
+        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer ")
 
