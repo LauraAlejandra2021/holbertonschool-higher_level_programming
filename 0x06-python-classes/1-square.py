cb36c8aab26025831f62f38a1786be748f4fcc3a
LauraAlejandra2021 <3450@holbertonschool.com>
2021-09-28 22:08:50 -0500

    square constructor

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square constructor"""
+
+
+class Square:
+    """square constructor"""
+
+    def __init__(self, size=0):
+        """ private square constructor """
+        self.__size=size
