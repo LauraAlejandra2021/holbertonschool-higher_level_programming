c905ce2282952f830ada224ea0a6d45407a1ff0d
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-05 13:12:46 -0500

    desarrollo parcial

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,2 +1,13 @@
+#!/usr/bin/python3
 def add_integer(a, b=98):
-    
+    """ Addition function """
+    if  type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    if type(a) is not int:
+        raise TypeError ("a must be an integer")
+    if type(b) is not int:
+        raise TypeError ("a must be an integer")
+        return a+b 
+        
