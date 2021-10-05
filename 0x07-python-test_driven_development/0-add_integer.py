4633f01b9322eb694eac25a41705fe4c502ce065
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-05 17:37:51 -0500

    test add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
+"""
+this function adds
+two numbers of type
+int or float
+"""
+
+
 def add_integer(a, b=98):
-    """ Addition function """
-    if  type(a) is float:
+    """
+  pass parameters of another type to int or float
+    """
+    if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
     if type(a) is not int:
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int:
-        raise TypeError ("a must be an integer")
-        return a+b 
-        
+        raise TypeError("a must be an integer")
+    return a+b
