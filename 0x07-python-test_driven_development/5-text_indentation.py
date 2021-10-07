5f514ec502cead003145ee1bef7ef5b65faf1a0d
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-06 23:53:16 -0500

    text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this function tokenizes
+a text according to
+requirements
+"""
+
+
+def text_indentation(text):
+    """tokenize a text"""
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        if text[i] == ".":
+            print(".\n")
+        elif text[i] == "?":
+            print('?\n')
+        elif text[i] == ":":
+            print(":\n")
+        else:
+            if (text[i] == " " and (text[i - 1] in ".?:"
+                                    or text[i - 1] == " ")):
+                continue
+            print("{}".format(text[i]), end="")
