aa5f5ff8feecd03d1930b0b0d764359ce6db890b
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-06 22:52:13 -0500

    print square, int

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+this function prints
+a square, taking 
+only whole numbers
+"""
+
+
+def print_square(size):
+
+    """
+    this function prints a square
+    
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is not int and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        for j in range(size):
+            print("#", end='')
+        print("")
