3296b14a0521715ba72ba36498a1aa8c8bd09237
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-11 19:17:44 -0500

    Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This function returns a list 
+of attributes and methods 
+of an object
+"""
+def lookup(obj):
+    """ 
+    returns list of attributes
+    """
+    return dir(obj)
