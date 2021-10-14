89d3d92d5e54d75900f3c7cb59b6efaf8e47a9b5
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-13 23:42:16 -0500

    class inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+function that inherits
+a class to use
+its method
+
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        """
+        verifying with the method integer validator
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
