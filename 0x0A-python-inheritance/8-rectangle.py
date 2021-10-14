2a33f6bb012e1d9f24a4a42a2776a88a5999c1bb
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-14 00:28:58 -0500

    ajustes codigo

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
-        new class
+       a new class
     """
 
     def __init__(self, width, height):
