3b3bcc9b06facdf156f358b8423187d3cc587e0a
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-13 22:02:36 -0500

    compare an object of a class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""""
+function that tells me 
+if an objero is an 
+instance of a class
+
+"""
+
+def is_same_class(obj, a_class):
+    """
+    compare an object of a class
+    """
+    if type(obj) == a_class:
+        return True
+    else: 
+        return False 
