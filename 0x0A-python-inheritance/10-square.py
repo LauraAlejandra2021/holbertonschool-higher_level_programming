dc70a7a4988212266a847f8d9822f7bb90167e35
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-14 16:03:45 -0500

    create new class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+class that inherits the class rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+       a new class
+    """
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
