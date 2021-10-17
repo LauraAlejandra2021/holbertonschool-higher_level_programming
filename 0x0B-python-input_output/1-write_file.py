e9cc08e3802b754143fec5279bfa53053e070be6
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-17 16:38:22 -0500

    write file (utf8)

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+function that
+reads aUTF8 file,
+returns the number of characters
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    function that reads a UTF8 f
+    returns the number of characters
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
+  
