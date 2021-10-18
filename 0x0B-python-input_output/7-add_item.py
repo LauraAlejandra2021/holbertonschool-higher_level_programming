2f063b6669b3982c5086cc001accbb985ba74f96
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-18 13:17:18 -0500

    comments 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,7 +11,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 my_lyst1 = []
-
+"""
+a list the saved Json representation
+"""
 
 if os.path.exists("add_item.json") is True:
     my_lyst1 = list(load_from_json_file("add_item.json"))
