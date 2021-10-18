f6619dad03660face46e99d137036588ca58be48
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-18 13:26:27 -0500

    dolor de cabeza

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,7 +5,7 @@ arguments to a python list
 and then saves them to a file
 """
 import sys
-import os.path
+import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -13,13 +13,13 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if os.path.exists("add_item.json") is True:
     my_lyst1 = list(load_from_json_file("add_item.json"))
-
-    for i in range(1, len(sys.argv)):
-        my_lyst1.append(sys.argv[i])
-    save_to_json_file(my_lyst1, "add_item.json")
     """
     a list the saved Json representation
     """
+    for i in range(1, len(sys.argv)):
+        my_lyst1.append(sys.argv[i])
+    save_to_json_file(my_lyst1, "add_item.json")
+   
 else:
     my_lyst1 = []
     save_to_json_file(my_lyst1, "add_item.json")
