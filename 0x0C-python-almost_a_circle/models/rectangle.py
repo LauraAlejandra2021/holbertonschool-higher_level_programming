38ee7f87ae5016187733c209b00973ee0113164b
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-18 18:36:11 -0500

    create and inherit classes, attribute definition and attribute validation

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+"""
+create
+a class
+rectangle
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """constructor method rectangle
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """getter of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter of width"""
+        self.__width = value
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+    @property
+    def height(self):
+        """getter of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter of height"""
+        self.__height = value
+        
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
+    @property
+    def x(self, value):
+        """getter x"""
+        return self.__x
+
+    @x.setter
+    def x(self,value):
+        """setter x"""
+        self.__x = value
+
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+            
+
+    @property
+    def y(self):
+        """getter y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """setter y"""
+        self.__y = value
+
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
