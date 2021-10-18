75577e90ef48c5d02dd8e762458403f4275df270
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-18 18:50:44 -0500

    pycodestyle point 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,15 +4,17 @@ create
 a
 superclass
 """
+
+
 class Base:
     """
     class base, and constructor method
     """
-
     __nb_objects = 0
+
     def __init__(self, id=None):
         if id is not None:
-            self.id=id
+            self.id = id
         else:
-            Base.__nb_objects += 1 
+            Base.__nb_objects += 1
             self.id = Base.__nb_objects
