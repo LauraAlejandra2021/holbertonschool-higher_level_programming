6562e809ec61407d442170b50201f593ea585b03
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-18 20:44:36 -0500

    split a line of code

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,5 +96,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """ void str"""
-        return(f"[Rectangle]\
+        return(f"[Rectangle] \
 ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}")
