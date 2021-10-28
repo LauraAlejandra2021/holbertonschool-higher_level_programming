7725b6e9f388761b1425a03bef24670a38ede5a4
LauraAlejandra2021 <3450@holbertonschool.com>
2021-10-28 12:25:47 -0500

    import rectangle attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,8 +12,14 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ new class"""
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         """constructor method square
         """
-      
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    def __str__(self):
+        """ void str """
+        return(f"[Square] \
+({self.id}) {self.x}/{self.y} - /{self.height}")
---
+++ 0x0C-python-almost_a_circle/sum.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+def add (b, a):
+    return b + a
