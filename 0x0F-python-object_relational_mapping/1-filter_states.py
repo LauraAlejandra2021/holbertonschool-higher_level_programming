bc0ff3b17e3d514416feb98f13f1ce7dd8dcf0e3
LauraAlejandra2021 <3450@holbertonschool.com>
2021-12-18 15:51:14 -0500

    filter_states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-   Script that lists all states from the database hbtn_0e_0_usa
+   script that lists all states with a name starting with N (upper N)
+   from the database hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
@@ -10,7 +11,8 @@ if __name__ == "__main__":
                          user=argv[1], password=argv[2],
                          db=argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
     for row in cursor.fetchall():
         print(row)
     cursor.close()
