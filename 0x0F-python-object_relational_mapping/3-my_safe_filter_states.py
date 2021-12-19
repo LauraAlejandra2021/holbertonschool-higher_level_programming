b22b092cb3e1f34d8a5498f06d020f69bb5eb736
LauraAlejandra2021 <3450@holbertonschool.com>
2021-12-18 19:07:38 -0500

    delete security

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+   delete security
+"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], password=argv[2],
+                         db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE\
+    name= %s ORDER BY id ASC", (argv[4], ))
+    for row in cursor.fetchall():
+        print(row)
+    cursor.close()
+    db.close()
